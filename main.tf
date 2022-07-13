c81dc2fda333668bfa735aaf2a633f4dae15595a
Rajeev Ghosh <rajeevkghosh@gmail.com>
2022-07-13 18:02:05 +0530

    updated code

--- main.tf
+++ main.tf
@@ -22,4 +22,83 @@ resource "google_kms_crypto_key" "cryptokey-25" {
   }
 }
 
+resource "google_kms_crypto_key" "cryptokey-100" {
+  name     = "cryptokey-100"
+  purpose  = "ASYMMETRIC_SIGN"
+  key_ring = data.google_kms_key_ring.keyring-1.id
+  version_template {
+    algorithm = "RSA_SIGN_PSS_2048_SHA256"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "google_kms_crypto_key" "cryptokey-101" {
+  name     = "cryptokey-101"
+  purpose  = "ASYMMETRIC_SIGN"
+  key_ring = data.google_kms_key_ring.keyring-1.id
+  version_template {
+    algorithm = "RSA_SIGN_PSS_3072_SHA256"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "google_kms_crypto_key" "cryptokey-102" {
+  name     = "cryptokey-102"
+  purpose  = "ASYMMETRIC_SIGN"
+  key_ring = data.google_kms_key_ring.keyring-1.id
+  version_template {
+    algorithm = "RSA_SIGN_PSS_4096_SHA256"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "google_kms_crypto_key" "cryptokey-103" {
+  name     = "cryptokey-103"
+  purpose  = "ASYMMETRIC_SIGN"
+  key_ring = data.google_kms_key_ring.keyring-1.id
+  version_template {
+    algorithm = "RSA_SIGN_PKCS1_2048_SHA256"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "google_kms_crypto_key" "cryptokey-104" {
+  name     = "cryptokey-104"
+  purpose  = "ASYMMETRIC_SIGN"
+  key_ring = data.google_kms_key_ring.keyring-1.id
+  version_template {
+    algorithm = "RSA_SIGN_PKCS1_3072_SHA256"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "google_kms_crypto_key" "cryptokey-105" {
+  name     = "cryptokey-105"
+  purpose  = "ASYMMETRIC_SIGN"
+  key_ring = data.google_kms_key_ring.keyring-1.id
+  version_template {
+    algorithm = "RSA_SIGN_PKCS1_4096_SHA256"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+
 
