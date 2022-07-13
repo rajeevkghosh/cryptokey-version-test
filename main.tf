dc75f623751df651fe613189710ddb22519089be
Rajeev Ghosh <rajeevkghosh@gmail.com>
2022-07-13 16:54:56 +0530

    updated code

--- main.tf
+++ main.tf
@@ -1,19 +1,24 @@
+
+provider "google" {
+    //credentials = file ("../composer-sa.json")
+    project = "modular-scout-345114"
+}
 data "google_kms_key_ring" "keyring-1" {
   name     = "keyring-1"
   location = "us-central1"
   project = "modular-scout-345114"
 }
 
-resource "google_kms_crypto_key" "cryptokey-22" {
-  name     = "cryptokey-22"
+resource "google_kms_crypto_key" "cryptokey-25" {
+  name     = "cryptokey-25"
   key_ring = data.google_kms_key_ring.keyring-1.id
-  rotation_period = "100000s"
+  version_template {
+    algorithm = "RSA_PSS_2048_SHA256"
+  }
 
   lifecycle {
     prevent_destroy = false
   }
 }
 
-data "google_kms_crypto_key_version" "crypto_key_version" {
-  crypto_key = google_kms_crypto_key.cryptokey-22.id
-}
+
